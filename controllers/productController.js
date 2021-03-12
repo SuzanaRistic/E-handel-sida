7980ae793c098c11eaaa09f77acdf5f4a9f33d53
PegJac <peggy.jacobson@outlook.com>
2021-03-12 11:51:33 +0100

    Pull

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,20 +1,20 @@
 const Product = require("../models/productSchema")
 
-const plantGET = (req,res) => {
+const plantGET = (req, res) => {
     res.render("product.ejs");
 }
-const cactusGET = (req,res) => {
-  const cactus = Product.find({category: "cactuses"})  
-  res.render("product.ejs", {product:cactus});
-  console.log(cactus);
+const cactusGET = (req, res) => {
+    const cactus = Product.find({ category: "cactuses" })
+    res.render("product.ejs", { product: cactus });
+    //console.log(cactus);
 }
-const tulipGET = (req,res) => {
+const tulipGET = (req, res) => {
     res.render("product.ejs");
 }
-const bouquetGET = (req,res) => {
+const bouquetGET = (req, res) => {
     res.render("product.ejs");
 }
-const vaseGET = (req,res) => {
+const vaseGET = (req, res) => {
     res.render("product.ejs");
 }
 
