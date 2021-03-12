964157f38a421b1a6927068bee38beb5b3a9eeed
max-da <max.dahlbom@outlook.com>
2021-03-12 14:29:42 +0100

    pagination, sorting, parpromgrammerat av alla

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,33 +1,54 @@
-const Product = require("../models/productSchema")
+const Product = require("../models/productSchema");
 
-const plantGET = async (req,res) => {
-    const plants = await Product.find({category:"plants"})  
-    res.render("product.ejs", {product:plants});
-}
-const cactusGET = async (req,res) => {
-  const cactuses = await Product.find({category:"cactuses"})  
-  res.render("product.ejs", {product:cactuses});
-  //console.log(cactus);
-}
-const tulipGET = async (req,res) => {
-    const tulips = await Product.find({category:"tulips"})  
-    res.render("product.ejs", {product:tulips});
-}
-const bouquetGET = async (req,res) => {
-    const bouquets = await Product.find({category:"bouquets"})  
-    res.render("product.ejs", {product:bouquets});
-}
-const vaseGET = async (req,res) => {
-    const vases = await Product.find({category:"vases"})  
-    res.render("product.ejs", {product:vases});
-}
+const plantGET = async (req, res) => {
+  pagination(req, res, "plants");
+};
+const cactusGET = async (req, res) => {
+  pagination(req, res, "cactuses");
 
-
+};
+const tulipGET = async (req, res) => {
+  pagination(req, res, "tulips");
+};
+const bouquetGET = async (req, res) => {
+  pagination(req, res, "bouquets");
+};
+const vaseGET = async (req, res) => {
+  pagination(req, res, "vases");
+};
 
 module.exports = {
-    plantGET,
-    cactusGET,
-    tulipGET,
-    bouquetGET,
-    vaseGET
+  plantGET,
+  cactusGET,
+  tulipGET,
+  bouquetGET,
+  vaseGET,
+};
+
+async function pagination(req, res, category) {
+  const page = +req.query.page || 1;
+  const sorted = +req.query.sorted || 1;
+
+  try {
+    const dataPerPage = 2;
+    const dataToShow = page + page;
+    const plants = await Product.find({ category: category })
+      .limit(dataToShow)
+      .sort({ name: sorted });
+    const totalData = await Product.find({
+      category: category,
+    }).countDocuments();
+
+
+    res.render("product.ejs", {
+      product: plants,
+      dataToShow: dataToShow,
+      dataPerPage: dataPerPage,
+      totalData: totalData,
+      page: page,
+    });
+  } catch (err) {
+    console.log("ERRRO");
+     res.send("asdasd");
+  }
 }
