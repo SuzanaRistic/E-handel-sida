f8938c39c30f6254ba3855c82a0edf99279e5054
max-da <max.dahlbom@outlook.com>
2021-03-23 09:38:02 +0100

    shop test

--- middleware/shoppingCart.js
+++ middleware/shoppingCart.js
@@ -2,33 +2,41 @@ const jwt = require("jsonwebtoken");
 require("dotenv").config();
 const mongoose = require("mongoose");
 const Cart = require("../models/cartSchema");
-const User = require("../models/userSchema")
+const User = require("../models/userSchema");
 //Dekodar jwt för att ge tillgång till username osv
 
-const shoppingCart = async(req, res, next)=>
-
-
-{
-try{
-
+const shoppingCart = async (req, res, next) => {
+  try {
     const token = req.cookies.jwToken;
-    
-    const decoded = jwt.verify(token, process.env.TOKEN_KEY)
-    
-    const user = await User.findOne({email: decoded.user.email})
 
-    let cart = await Cart.findOne({ userId: user._id }).populate("products.productId");
-  
+    const decoded = jwt.verify(token, process.env.TOKEN_KEY);
+
+    const user = await User.findOne({ email: decoded.user.email });
+
+    let cart = await Cart.findOne({ userId: user._id }).populate(
+      "products.productId"
+    );
+    console.log(cart.products);
+   
+
+    //console.log(cart.products)
     req.shoppingCart = cart.products;
-    req.email = decoded.user.email
+    req.email = decoded.user.email;
     req.userFull = decoded;
-}
-catch(err){
-    console.log(err)
-}
+  } catch (err) {
+    console.log(err);
+  }
 
-next()
-
-}
+  next();
+};
 
 module.exports = shoppingCart;
+
+/* async function loop(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    let productTotal = arr[i].quantity * arr[i].productId.price;
+    console.log(productTotal);
+    return productTotal;
+  }
+}
+ */
