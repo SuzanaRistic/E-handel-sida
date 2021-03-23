85acaecfbb6770da520fa904363a5d0db26b828f
max-da <max.dahlbom@outlook.com>
2021-03-23 14:13:31 +0100

    merge

--- middleware/shoppingCart.js
+++ middleware/shoppingCart.js
@@ -2,27 +2,6 @@ const jwt = require("jsonwebtoken");
 require("dotenv").config();
 const mongoose = require("mongoose");
 const Cart = require("../models/cartSchema");
-<<<<<<< HEAD
-const { User } = require("../models/userSchema")
-//Dekodar jwt för att ge tillgång till username osv
-
-const shoppingCart = async (req, res, next) => {
-    try {
-        const token = req.cookies.jwToken;
-        const decoded = jwt.verify(token, process.env.TOKEN_KEY)
-        const user = await User.findOne({ email: decoded.user.email })
-        let cart = await Cart.findOne({ userId: user._id }).populate("products.productId");
-
-        req.shoppingCart = cart.products;
-        req.email = decoded.user.email
-        req.userFull = decoded;
-    }
-    catch (err) {
-        console.log(err)
-    }
-    next()
-}
-=======
 const User = require("../models/userSchema");
 //Dekodar jwt för att ge tillgång till username osv
 
@@ -58,7 +37,6 @@ const shoppingCart = async (req, res, next) => {
 };
 
 module.exports = shoppingCart;
->>>>>>> cartDev
 
 async function loop(cart) {
   for (let i = 0; i < cart.products.length; i++) {
