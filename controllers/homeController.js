21b34e3016ffb647a047cc7a80db8d5849496d33
max-da <max.dahlbom@outlook.com>
2021-03-30 15:08:52 +0200

    cartsss

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,6 +1,6 @@
 const homeGET = (req, res) => {
     let token = req.cookies.jwToken;
-    res.render("index.ejs", { error: "", token})
+    res.render("index.ejs", { error: "", token,shoppingCart:req.shoppingCart})
 }
 
 module.exports = homeGET;
--- routes/homeRouter.js
+++ routes/homeRouter.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const homeGET = require("../controllers/homeController");
+const shoppingCart = require("../middleware/shoppingCart");
 
-router.get("/", homeGET)
+router.get("/", shoppingCart, homeGET)
 
 module.exports = router;
